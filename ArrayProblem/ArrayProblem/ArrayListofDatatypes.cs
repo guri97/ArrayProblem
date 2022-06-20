ef5e01a73a98d00a3f9e4b54ad3824a945cfa2d1
guri97 <105732963+guri97@users.noreply.github.com>
2022-06-20 22:55:40 +0530

    [gurpreet] UC1_ArrayofIntDoubleChar

---
+++ ArrayProblem/ArrayProblem/ArrayListofDatatypes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13_Array
+    {
+
+        public class ArrayListOfDataTypes
+        {
+            int[] intArray = { 1, 2, 3, 4, 5, 6, 7 };
+            public void PrintIntArray()
+            {
+                foreach (var element in intArray)
+                {
+                    Console.WriteLine(element);
+                }
+                Console.WriteLine("------------------------------------");
+            }
+
+            double[] doubleArray = { 1.2, 1.3, 1.4, 1.5 };
+            public void PrintDoubleArray()
+            {
+                foreach (var element in doubleArray)
+                {
+                    Console.WriteLine(element);
+                }
+                Console.WriteLine("------------------------------------");
+            }
+
+            char[] charArray = { 'A', 'B', 'C', 'D' };
+            public void PrintCharArray()
+            {
+                foreach (var element in charArray)
+                {
+                    Console.WriteLine(element);
+                }
+                Console.WriteLine("------------------------------------");
+            }
+
+        }
+
+    }
+
---
+++ ArrayProblem/ArrayProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Day13_Array
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ArrayListOfDataTypes arrayListOfData = new ArrayListOfDataTypes();
+            arrayListOfData.PrintIntArray();
+            arrayListOfData.PrintDoubleArray();
+            arrayListOfData.PrintCharArray();
+            Console.ReadLine();
+        }
+    }
+}
