b3690d2484de28211b263515904e925aa802da75
guri97 <105732963+guri97@users.noreply.github.com>
2022-06-20 23:16:49 +0530

    [gurpreet] UC3_ArrayListWithGenericClass

---
+++ ArrayProblem/ArrayProblem/ArrayListWithGenericClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13_Array
+{
+    public class ArrayListUsingGenericClass<T>
+    {
+        public T[] inputArray;
+        public ArrayListUsingGenericClass(T[] inputArray)
+        {
+            this.inputArray = inputArray;
+        }
+        public void ToPrint()
+        {
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("-----------------------------------");
+        }
+    }
+}
--- ArrayProblem/ArrayProblem/Program.cs
+++ ArrayProblem/ArrayProblem/Program.cs
@@ -10,19 +10,21 @@
             arrayListOfData.PrintDoubleArray();
             arrayListOfData.PrintCharArray();
 
-
-
-
             int[] intArray = { 1, 2, 3, 4, 5, 6, 7 };
             double[] doubleArray = { 1.2, 1.3, 1.4, 1.5 };
             char[] charArray = { 'A', 'B', 'C', 'D' };
             ArrayListWithGenerics arrayListWithGenerics = new ArrayListWithGenerics();
+
+
             Console.WriteLine("Using Generic Method: ");
             arrayListWithGenerics.ToprintGeneric<int>(intArray);
             arrayListWithGenerics.ToprintGeneric<double>(doubleArray);
             arrayListWithGenerics.ToprintGeneric<char>(charArray);
 
-            Console.ReadLine();
+            Console.WriteLine("Using Generic Classes: ");
+            new ArrayListUsingGenericClass<int>(intArray).ToPrint();
+            new ArrayListUsingGenericClass<double>(doubleArray).ToPrint();
+            new ArrayListUsingGenericClass<char>(charArray).ToPrint();
         }
     }
 }
