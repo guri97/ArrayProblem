ca7ebd76b987a050886e194fdb7fd92884cda07d
guri97 <105732963+guri97@users.noreply.github.com>
2022-06-20 23:08:23 +0530

    [gurpreet] UC2_ArrayUsingGenericMethod

--- ArrayProblem/ArrayProblem/ArrayListofDatatypes.cs
+++ ArrayProblem/ArrayProblem/ArrayListofDatatypes.cs
@@ -5,41 +5,40 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Day13_Array
+{
+
+    public class ArrayListOfDataTypes
     {
-
-        public class ArrayListOfDataTypes
+        int[] intArray = { 1, 2, 3, 4, 5, 6, 7 };
+        public void PrintIntArray()
         {
-            int[] intArray = { 1, 2, 3, 4, 5, 6, 7 };
-            public void PrintIntArray()
+            foreach (var element in intArray)
             {
-                foreach (var element in intArray)
-                {
-                    Console.WriteLine(element);
-                }
-                Console.WriteLine("------------------------------------");
+                Console.WriteLine(element);
             }
+            Console.WriteLine("------------------------------------");
+        }
 
-            double[] doubleArray = { 1.2, 1.3, 1.4, 1.5 };
-            public void PrintDoubleArray()
+        double[] doubleArray = { 1.2, 1.3, 1.4, 1.5 };
+        public void PrintDoubleArray()
+        {
+            foreach (var element in doubleArray)
             {
-                foreach (var element in doubleArray)
-                {
-                    Console.WriteLine(element);
-                }
-                Console.WriteLine("------------------------------------");
+                Console.WriteLine(element);
             }
+            Console.WriteLine("------------------------------------");
+        }
 
-            char[] charArray = { 'A', 'B', 'C', 'D' };
-            public void PrintCharArray()
+        char[] charArray = { 'A', 'B', 'C', 'D' };
+        public void PrintCharArray()
+        {
+            foreach (var element in charArray)
             {
-                foreach (var element in charArray)
-                {
-                    Console.WriteLine(element);
-                }
-                Console.WriteLine("------------------------------------");
+                Console.WriteLine(element);
             }
-
+            Console.WriteLine("------------------------------------");
         }
 
     }
 
+}
--- ArrayProblem/ArrayProblem/Program.cs
+++ ArrayProblem/ArrayProblem/Program.cs
@@ -5,9 +5,23 @@
         public static void Main(string[] args)
         {
             ArrayListOfDataTypes arrayListOfData = new ArrayListOfDataTypes();
+            Console.WriteLine("Using Normal Method: ");
             arrayListOfData.PrintIntArray();
             arrayListOfData.PrintDoubleArray();
             arrayListOfData.PrintCharArray();
+
+
+
+
+            int[] intArray = { 1, 2, 3, 4, 5, 6, 7 };
+            double[] doubleArray = { 1.2, 1.3, 1.4, 1.5 };
+            char[] charArray = { 'A', 'B', 'C', 'D' };
+            ArrayListWithGenerics arrayListWithGenerics = new ArrayListWithGenerics();
+            Console.WriteLine("Using Generic Method: ");
+            arrayListWithGenerics.ToprintGeneric<int>(intArray);
+            arrayListWithGenerics.ToprintGeneric<double>(doubleArray);
+            arrayListWithGenerics.ToprintGeneric<char>(charArray);
+
             Console.ReadLine();
         }
     }
